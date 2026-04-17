043142c7b8e4acf559fb73b43154d8be88b88da3
Diya Sicily Siju <diyasicilysiju@gmail.com>
2026-04-17 14:10:54 +0530

    new

--- library.py
+++ library.py
@@ -1,9 +1,2 @@
 print("Library Management System")
 print("Student Registration Module Added")
-
-def login():
-<<<<<<< HEAD
-    print("Student Login Feature Enabled")
-=======
-    print("Student Login Feature Enabled with OTP")
->>>>>>> login-feature
